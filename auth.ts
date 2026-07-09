29fb3b7058859cf14321cf146147037ca862b7b8
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2026-07-09 17:20:32 +0530

    FEAT : authentication using credentials

--- auth.ts
+++ auth.ts
@@ -1,10 +1,63 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
 
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "@/lib/prisma"
- 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
-})
+  providers: [
+    Google,
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const email = credentials?.email as string;
+        const password = credentials?.password as string;
+
+        if (!email || !password) {
+          console.log("Email or Password can't be empty");
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email },
+        });
+
+        if (!user) {
+          console.log("User not found");
+          return null;
+        }
+
+        if (!user.password) {
+          console.log("Password login is not available for this account");
+          return null;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+          console.log("Invalid Password");
+          return null;
+        }
+
+        console.log("Login successful");
+        return {
+          ...user,
+          id: String(user.id),
+        };
+      },
+    }),
+  ],
+  // callbacks: {
+  //   authorized: async ({ auth }) => {
+  //     return !!auth;
+  //   },
+  // },
+  session: {
+    strategy: "jwt",
+  },
+});
